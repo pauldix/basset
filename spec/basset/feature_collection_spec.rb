5c39afd5b6b45e9497ebc206e6b3b273bf7baf9e
Paul Dix <paul@pauldix.net>
2009-10-30 17:53:39 -0400

    made spec a little cleaner

--- spec/basset/feature_collection_spec.rb
+++ spec/basset/feature_collection_spec.rb
@@ -2,28 +2,25 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "feature collection" do
   describe "numbering features" do
-    it "takes rows of features" do
+    before(:each) do
       @collection = Basset::FeatureCollection.new
       @collection.add_row %w[hello paul]
+    end
+    
+    it "takes rows of features" do
       @collection.features.should == %w[hello paul]
     end
     
     it "keeps the index of a feature" do
-      @collection = Basset::FeatureCollection.new
-      @collection.add_row %w[hello paul]
       @collection.index_of("hello").should == 0
       @collection.index_of("paul").should == 1
     end
     
     it "returns nil as the index for a feature not in the collection" do
-      @collection = Basset::FeatureCollection.new
-      @collection.add_row %w[hello paul]
       @collection.index_of("whatevs").should == nil
     end
     
     it "knows the number of times a feature occurs across all rows" do
-      @collection = Basset::FeatureCollection.new
-      @collection.add_row %w[hello paul]
       @collection.add_row %w[code hello paul and paul]
       @collection.global_frequency("hello").should == 2
       @collection.global_frequency("paul").should == 3

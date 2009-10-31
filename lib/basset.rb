2d7db584e6de41196b0476b66835a31817f885f7
Paul Dix <paul@pauldix.net>
2009-10-30 20:22:07 -0400

    fixed load paths and updated the gemspec with the proper version

--- lib/basset.rb
+++ lib/basset.rb
@@ -1,8 +1,10 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__))
+
 module Basset; end;
 
-require 'lib/basset/parser'
-require 'lib/basset/feature_collection'
-require 'lib/basset/vector_collection'
+require 'basset/parser'
+require 'basset/feature_collection'
+require 'basset/vector_collection'
 
 module Basset
   VERSION = "2.0.0"
